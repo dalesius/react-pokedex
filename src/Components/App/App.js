aa4904e9038072114d76edbe7b212113f29ddd35
Dalo <dali.molochnik@gmail.com>
2020-07-22 16:45:11 -0300

    fix: Unique fetch moved to index and working properly

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,51 +1,13 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import "./App.css";
 import PokemonsList from "../PokemonsList/pokemons_list";
 import SearchBar from "../SearchBar/search_bar";
 
-function App() {
-  const [pokemonsData, setPokemonsData] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
+function App(props) {
+  const pokemons = props.pokemons;
   const [filterText, setFilterText] = useState("");
 
-  console.log(pokemonsData);
-  useEffect(() => {
-    async function fetchData() {
-      try {
-        setIsLoading(true);
-        let pokemonsUrls = await fetchPokemonsUrls(
-          "https://pokeapi.co/api/v2/pokemon/?limit=151"
-        );
-
-        // Looping through all urls to get the data
-        let pokemonsList = [];
-        await pokemonsUrls.forEach(async (url) => {
-          let pokemon = await fetchPokemon(url);
-          pokemonsList.push(pokemon);
-        });
-
-        setPokemonsData(pokemonsList);
-        setIsLoading(false);
-      } catch (error) {
-        console.log(error.message);
-        return error.message;
-      }
-    }
-    fetchData();
-  }, []);
-
-  // Cargo spinner o texto hasta que se dispare useEffect
-  if (isLoading) {
-    return (
-      <div className="container-fluid">
-        <SearchBar
-          filtertext={filterText}
-          onInputChange={(text) => setFilterText(text)}
-        />
-        <p>Loading ...</p>
-      </div>
-    );
-  }
+  console.log(pokemons);
 
   // Cargo la pokedex
   return (
@@ -54,52 +16,9 @@ function App() {
         filtertext={filterText}
         onInputChange={(text) => setFilterText(text)}
       />
-      <PokemonsList pokemonslist={pokemonsData} filtertext={filterText} />
+      <PokemonsList pokemonslist={pokemons} filtertext={filterText} />
     </div>
   );
-
-  // Fetches the needed info for creating a pokemon object
-  // returns a Pokemon object.
-  async function fetchPokemon(pokeUrl) {
-    try {
-      // Fetching pokemon data
-      let pokemon = await fetch(pokeUrl)
-        // Parsing into json
-        .then((rawPoke) => rawPoke.json())
-        // Keeping only the needed data
-        .then((pokeData) => {
-          const pokeobj = {
-            id: pokeData.id,
-            name: pokeData.name,
-            types: pokeData.types.map((value) => value.type.name),
-            img: `https://pokeres.bastionbot.org/images/pokemon/${pokeData.id}.png`,
-          };
-          return pokeobj;
-        });
-      return pokemon;
-    } catch (error) {
-      console.log(error.message);
-    }
-  }
-
-  // Fetches all the pokemon urls which contain the needed data
-  // returns an array of urls.
-  async function fetchPokemonsUrls(apiUrl) {
-    try {
-      let pokemonsUrls = await fetch(apiUrl)
-        // Parsing the data into json
-        .then((rawData) => rawData.json())
-        // Keeping only the pokemons data
-        .then((jsonData) => jsonData.results)
-        // Trim all the data to keep the url
-        .then((pokeUrls) => {
-          return pokeUrls.map((pokeUrls) => pokeUrls.url);
-        });
-      return pokemonsUrls;
-    } catch (error) {
-      console.log(error.message);
-    }
-  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -5,11 +5,55 @@ import "./index.css";
 import App from "./Components/App/App";
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+// Fetching pokemons urls
+const pokeUrls = fetch("https://pokeapi.co/api/v2/pokemon/?limit=151")
+  .then((response) => response.json())
+  // Keeping only the pokemons url
+  .then((jsonData) => {
+    const pokeUrls = jsonData.results;
+    // Trim all the data to keep the url
+    return pokeUrls.map((pokeUrls) => pokeUrls.url);
+  })
+  .catch((error) => {
+    console.log(`Error while fetching pokemons urls: ${error}`);
+  });
+
+// Once data retrieved, load pokemons array
+const pokemonsArray = pokeUrls.then((urlsArray) => {
+  // Devuelvo una promesa con todas las promesas generadas en el forEach
+  return Promise.all(
+    urlsArray.map((url) => {
+      return (
+        fetch(url)
+          // Parsing into json
+          .then((response) => response.json())
+          // Keeping only the needed data
+          .then((pokeData) => {
+            const pokeobj = {
+              id: pokeData.id,
+              name: pokeData.name,
+              types: pokeData.types.map((value) => value.type.name),
+              img: `https://pokeres.bastionbot.org/images/pokemon/${pokeData.id}.png`,
+            };
+            return pokeobj;
+          })
+          .catch((error) => {
+            console.log(`Error while fetching each pokemons: ${error}`);
+          })
+      );
+    })
+  );
+});
+
+// After having the data, load the app
+pokemonsArray.then((pokemons) => {
+  console.log({ pokemons });
+  ReactDOM.render(
+    <React.StrictMode>
+      <App pokemons={pokemons} />
+    </React.StrictMode>,
+    document.getElementById("root")
+  );
+});
 
 serviceWorker.unregister();
